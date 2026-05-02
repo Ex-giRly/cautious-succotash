05261b9a1e340162c051367c688662f35c24e423
ALBA-Girly <Albaumeh@gmail.com>
2026-05-02 19:33:52 +0100

    Add files via upload

---
+++ login.py
@@ -0,0 +1,44 @@
+from tkinter import*
+
+root = Tk()
+root.title("Login app")
+root.geometry("300x350")
+
+frame = Frame(master=root, height  = 200, width = 360, bg = "lightgray")
+
+lbl1= Label(frame, text="Fullname", bg="pink",fg="black", width=12)
+lbl2= Label(frame,text = "Email Id", bg = "pink",fg="black", width=12)
+lbl3= Label(frame,text = "Enter Password", bg = "pink",fg="black", width=12)
+
+name_entry = Entry(frame)
+email_entry = Entry(frame)
+pass_entry = Entry(frame)
+
+def display():
+    name = name_entry.get()
+    greet = "Hey" + name
+    message = "\n Congratulations for your new account."
+    textbox.insert(END, greet)
+    textbox.insert(END, message)
+
+textbox = Text(bg="purple", fg="black")
+btn = Button(text = "Create Accountt", command=display, bg="yellow")
+frame.place(x=20,y=0)
+
+lbl1.place(x=20,y=20)
+
+name_entry.place(x=150,y=20)
+
+lbl2.place(x=20,y=80)
+
+email_entry.place(x=150,y=80)
+
+lbl3.place(x=20,y=140)
+
+pass_entry.place(x=130, y=140)
+
+btn.place(x=130, y=210)
+
+textbox.place(y=250)
+
+root.mainloop()
